07b72942ef12d8bcbcfdb428f0fa15534a84174e
saumyaj1 <saumyaj202@gmail.com>
2022-10-26 02:34:29 -0500

    fix url

--- src/App.js
+++ src/App.js
@@ -85,7 +85,7 @@ function HWSet(props){
     else{
       setState(0); 
     }
-    let url = 'https://sheltered-plains-55782.herokuapp.com/CheckOut/${props.name}/${y}'; 
+    let url = `https://sheltered-plains-55782.herokuapp.com/CheckOut/${props.name}/${y}`; 
     fetch(url).then((response) => response.json().then(value => {
       let quantity = value["Quantity"];
       alert(quantity)
